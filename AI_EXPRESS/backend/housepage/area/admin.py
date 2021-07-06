af9b930e942bb449021e0ee3b8636e76dbdaff1e
Minseung Park <31756398+withseungryu@users.noreply.github.com>
2021-07-06 20:45:32 +0900

    Delete AI_EXPRESS/backend/housepage/area directory

--- AI_EXPRESS/backend/housepage/area/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Dong
-from .models import SearchKeyword
-
-# Register your models here.
-admin.site.register(Dong)
-admin.site.register(SearchKeyword)
